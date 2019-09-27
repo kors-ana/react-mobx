2d5369bef20c16e1d75fdc07fb1d76a2c381e8ce
anastasia.korsikova <nastcka@gmail.com>
2019-09-27 12:26:01 +0300

    added filter and add new todo function

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -3,8 +3,56 @@ import { observer } from "mobx-react";
 
 @observer
 export default class TodoList extends React.Component {
+  filter(e) {
+    this.props.store.filter = e.target.value;
+  }
+
+  createNew(e) {
+    if (e.which === 13) {
+      this.props.store.createTodo(e.target.value);
+      e.target.value = "";
+    }
+  }
+
+  toggleComplete(todo) {
+    todo.complete = !todo.complete;
+  }
+
   render() {
-    console.log(this.props);
-    return <h1>{this.props.store.todos[0]}</h1>;
+    const { todos, filter, filteredTodos, clearComplete } = this.props.store;
+    return (
+      <div>
+        <h1>todos</h1>
+        <label>Enter new todos</label>
+        <br />
+        <input onKeyPress={this.createNew.bind(this)} />
+        <br />
+        <label>Enter the filter</label>
+        <br />
+        <input
+          className="filter"
+          value={filter}
+          onChange={this.filter.bind(this)}
+        />
+        <ul>
+          {filteredTodos.map(todo => {
+            return (
+              <li key={todo.id}>
+                <input
+                  type="checkbox"
+                  value={todo.complete}
+                  checked={todo.complete}
+                  onChange={this.toggleComplete.bind(this, todo)}
+                />
+                {todo.value}
+              </li>
+            );
+          })}
+        </ul>
+        <a href="#" onClick={clearComplete}>
+          Clear Complete
+        </a>
+      </div>
+    );
   }
 }
--- src/js/TodoStore.js
+++ src/js/TodoStore.js
@@ -1,8 +1,34 @@
-import { autorun, observable } from "mobx";
+import { computed, observable } from "mobx";
+
+class Todo {
+  @observable value;
+  @observable id;
+  @observable complete;
+
+  constructor(value) {
+    this.value = value;
+    this.id = Date.now();
+    this.complele = false;
+  }
+}
 
 class TodoStore {
-  @observable todos = ["buy milk", "get cheese"];
+  @observable todos = [];
   @observable filter = "";
+  @computed get filteredTodos() {
+    const matchesFilter = new RegExp(this.filter, "i");
+    return this.todos.filter(
+      todo => !this.filter || matchesFilter.test(todo.value)
+    );
+  }
+  createTodo(value) {
+    this.todos.push(new Todo(value));
+  }
+
+  clearComplete = () => {
+    const incompleteTodos = this.todos.filter(todo => !todo.complete);
+    this.todos.replace(incompleteTodos);
+  };
 }
 
 const store = new TodoStore();
